b8a4b94f926f67bb68d7c31e41051f6a971f77e3
A.Ch <cherednikov77@gmail.com>
2026-01-19 16:24:21 +0200

    Refactor draw_tree function for improved clarity and structure

--- Hw2.py
+++ Hw2.py
@@ -6,19 +6,28 @@ def draw_tree(t, l, depth):
     for  _ in range(4):
         t.forward(1)
         t.left(90)
-    t.forward(1)
+    new_l = l * math.sqrt(2) / 2
+    t.forward(new_l)
+    draw_tree(t, new_l, depth - 1)
+    t.backward(l)
     t.left(45)
-new_1 = 1 * math.sqrt(2) / 2
-t.right(90)
-t.forward(new_1)
-draw_tree(t, new_1, depth - 1)
-t.backward(new_1)
-t.left(45)
-t.backward(1)
-screen = turtle.Screen()
-screen.tracer(0)
+    t.backward(l)
+def main():
+    screen = turtle.Screen()
+    screen.tracer(0)
+    t = turtle.Turtle()
+    t.speed(0)
+    t.left(90)
 
-draw_tree(t, 100, 10)
+    draw_tree(t, 100, 10)
+    screen.update()
+    screen.mainloop()
+
+if __name__ == "__main__":
+    main()
 
 
-    
+    #####
+
+
+
